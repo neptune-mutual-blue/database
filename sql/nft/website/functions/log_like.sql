a06a87f3f364d0ddda1bf373462842ad9b654f56
binodnpm <tokyo@neptunemutual.com>
2023-06-05 00:16:34 +0400

    Fixed `log_like`
    Fixed bug in the `log_like` function.

--- sql/nft/website/functions/log_like.sql
+++ sql/nft/website/functions/log_like.sql
@@ -6,35 +6,41 @@ CREATE OR REPLACE FUNCTION log_like
 RETURNS uint256
 AS
 $$
-  DECLARE _previous                       boolean;
-  DECLARE _token_likes                    uint256;
+  DECLARE _previous                       boolean = false;
+  DECLARE _like_count                     uint256;
 BEGIN
   IF NOT EXISTS(SELECT 1 FROM nfts WHERE token_id = _token_id) THEN
     RAISE EXCEPTION 'Invalid token id';
   END IF;
+  
+  IF NOT EXISTS(SELECT * FROM likes WHERE token_id = _token_id AND liked_by = _account) THEN
+    INSERT INTO likes(token_id, liked_by, liked)
+    SELECT _token_id, _account, true;
+  END IF;
 
-  SELECT likes.liked                      INTO _previous
+  SELECT likes.liked
+  INTO _previous
   FROM likes
   WHERE likes.liked_by                    = _account
-  AND likes.token_id                      = _token_id
-  LIMIT 1;
+  AND likes.token_id                      = _token_id;
 
   UPDATE likes
   SET
     last_unliked_at                       = CASE WHEN liked = true  THEN NOW() ELSE last_unliked_at END,
     last_liked_at                         = CASE WHEN liked = false THEN NOW() ELSE last_liked_at   END,
-    liked                                 = NOT(COALESCE(liked, FALSE)) 
+    liked                                 = NOT(COALESCE(liked, false)) 
   WHERE likes.liked_by                    = _account
   AND likes.token_id                      = _token_id;
 
   UPDATE nfts
   SET likes                               = likes + (CASE WHEN _previous = true THEN -1 ELSE 1 END)
   WHERE token_id                          = _token_id
-  RETURNING nfts.likes                    INTO _token_likes;
+  RETURNING nfts.likes
+  INTO _like_count;
 
-  RETURN _token_likes;
+  RETURN _like_count;
 END
 $$
 LANGUAGE plpgsql;
 
--- SELECT * FROM log_like('0xB452AC021a1151AAF342c1B75aA914E03e6503b5', 100500)
+--SELECT * FROM log_like('0xB452AC021a1151AAF342c1B75aA914E03e6503b5', 100500)
