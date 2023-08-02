e9012438c7ae967dedc21256ebc9d2394346b990
flashburst <82953782+flashburst@users.noreply.github.com>
2023-08-02 18:41:30 +0530

    Updated NFT sql functions

--- sql/nft/functions.sql
+++ sql/nft/functions.sql
@@ -88,13 +88,52 @@ RETURNS address
 STABLE
 AS
 $$
+  DECLARE _owner                         address;
 BEGIN
-  RETURN "to"
-  FROM transfer_single
-  WHERE transfer_single.id = _token_id
-  ORDER BY transfer_single.block_timestamp DESC
+  SELECT "to" INTO _owner
+  FROM      transfer_single
+  WHERE     transfer_single.id = _token_id
+  ORDER BY  transfer_single.block_timestamp DESC
   LIMIT 1;
+
+  IF(_owner IS NOT NULL) THEN
+    RETURN _owner;
+  END IF;
+  
+  SELECT "account" INTO _owner
+  FROM      nft.soulbound_minted
+  WHERE     nft.soulbound_minted.token_id = _token_id
+  ORDER BY  nft.soulbound_minted.block_timestamp DESC
+  LIMIT 1;
+
+  RETURN _owner;
 END
 $$
 LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION get_nft_role(_token_id uint256)
+RETURNS address
+STABLE
+AS
+$$
+  DECLARE _nft_role                                           text;
+BEGIN
+  SELECT
+    CASE
+      WHEN nfts.family = 'Aquavallo'      THEN 'Guardian'
+      WHEN nfts.family = 'Merman Serpent' THEN 'Beast'
+      WHEN nfts.family = 'Gargantuworm'   THEN 'Beast'
+      WHEN nfts.family = 'Delphinus'      THEN 'Guardian'
+      WHEN nfts.family = 'Salacia'        THEN 'Guardian'
+      WHEN nfts.family = 'Grim Wyvern'    THEN 'Beast'
+      WHEN nfts.family = 'Neptune'        THEN 'Neptune'
+      WHEN nfts.family = 'Sabersquatch'   THEN 'Beast'
+    END                                                       INTO _nft_role
+  FROM nfts
+  WHERE nft.token_id = _token_id
+  LIMIT 1;
+
+  RETURN _nft_role;
+END
+$$
+LANGUAGE plpgsql;
