2155897bb503d864a383a26a72a4851c50175e1b
flashburst <82953782+flashburst@users.noreply.github.com>
2023-10-10 23:51:08 +0530

    Removed redundant quotes

--- sql/nft/website/marketplace/nft_bridge_transactions_view.sql
+++ sql/nft/website/marketplace/nft_bridge_transactions_view.sql
@@ -1,13 +1,13 @@
 CREATE OR REPLACE VIEW nft_bridge_transactions_view
 AS
 SELECT
-  "transaction_hash",
-  "block_timestamp",
-  "block_number",
-  "transaction_sender",
-  "chain_id",
-  "sender",
-  "receiver",
-  "token_ids",
-  "dst_chain_id"
+  transaction_hash,
+  block_timestamp,
+  block_number,
+  transaction_sender,
+  chain_id,
+  sender,
+  receiver,
+  token_ids,
+  dst_chain_id
 FROM nft.send_to_chain
